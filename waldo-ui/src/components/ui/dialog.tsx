d1a8fcd5ef4a0584deb27772bb0bf84be1677673
miguel <miguel@waldo.fyi>
2026-06-05 11:33:37 +0200

    fix: dialog description margin-bottom 24px before first body element

--- waldo-ui/src/components/ui/dialog.tsx
+++ waldo-ui/src/components/ui/dialog.tsx
@@ -130,7 +130,7 @@ const DialogDescription = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <DialogPrimitive.Description
     ref={ref}
-    className={cn("text-sm text-muted-foreground", className)}
+    className={cn("text-sm text-muted-foreground mt-1", className)}
     {...props}
   />
 ));
